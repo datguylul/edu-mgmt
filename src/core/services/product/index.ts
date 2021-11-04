7e9ebf8f0fbc495ea0e6ef0cec11ce412e366a4e
Nguyen Hoang <hoang.nguyen@fruitful.io>
2021-11-04 23:23:40 +0700

    try-update: move put to post

--- src/core/services/product/index.ts
+++ src/core/services/product/index.ts
@@ -1,8 +1,8 @@
 import { apiClient } from '../axiosInstance';
 import { ENDPOINTS } from '../../constants/endpoint';
 
-const { get, put } = apiClient;
+const { get, post, put } = apiClient;
 
 export const ProductList = (page: number, record: number) => get(`${ENDPOINTS.PRODUCTS}?page=${page}&record=${record}`);
 export const ProductDetail = (id: string) => get(`${ENDPOINTS.PRODUCTS}detail?id=${id}`);
-export const ProductUpdate = (params: {}) => put(ENDPOINTS.PRODUCTS_EDIT, params);
+export const ProductUpdate = (params: {}) => post(ENDPOINTS.PRODUCTS_EDIT, params);
